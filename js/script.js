db07c9200bac2a14362e95dfb6d270169c6f3389
johncabi <johnlloydcabiling14@gmail.com>
2026-01-09 08:57:14 +0800

    Update all, major changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
 const bibleVerseSpan = document.getElementById("bible-verse-span");
 const fetchNewVerseButton = document.getElementById("fetch-new-verse-button");
+const sidebarBtnOpen = document.getElementById("sidebar-btn-open");
+const sidebarBtnClose = document.getElementById("sidebar-btn-close");
+const aboutBtn = document.getElementById("about-btn");
+const aboutBtnIcon = document.getElementById("about-btn-icon");
+const modalBtnClose = document.getElementById("modal-btn-close");
+const aboutModal = document.querySelector("dialog");
+const sidebar = document.querySelector("aside");
+
+aboutBtn.addEventListener("click", openAboutModal);
+aboutBtnIcon.addEventListener("click", openAboutModal);
+
+function openAboutModal() {
+  aboutModal.showModal();
+}
+
+modalBtnClose.addEventListener("click", closeAboutModal);
+
+function closeAboutModal() {
+  aboutModal.close();
+}
+
+sidebarBtnOpen.addEventListener("click", openSidebar);
+
+function openSidebar() {
+  sidebar.classList.replace("translate-x-full", "translate-x-0");
+}
+
+sidebarBtnClose.addEventListener("click", closeSidebar);
+
+function closeSidebar() {
+  sidebar.classList.replace("translate-x-0", "translate-x-full");
+}
 
 fetchNewVerseButton.addEventListener("click", fetchNewVerse);
 
